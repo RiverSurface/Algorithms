ba886d79f9d149aa0ed90ed9b548d4018a36cb95
RiverSurface <riversurface@qq.com>
2017-09-05 09:00:05 +0800

    添加项目文件。

---
+++ Algorithms/include/headers.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <stdio.h>
+
+int EuclidGCD(int a, int b);
+void EuclidGCDTest(int a, int b);
---
+++ Algorithms/src/main.cpp
@@ -0,0 +1,8 @@
+#include "../include/headers.h"
+int main()
+{
+    EuclidGCDTest(252, 105);
+    //printf("Hello world!\n");
+    getchar();
+    return 0;
+}
